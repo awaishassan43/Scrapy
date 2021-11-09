50cb3bffa2265d4a007d977b778c992164355f35
Awais Hassan <awaishassan43@gmail.com>
2021-11-09 17:30:46 +0500

    initial comit

--- books/books/items.py
+++ books/books/items.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Define here the models for your scraped items
 #
 # See documentation in:
@@ -6,7 +8,7 @@
 import scrapy
 
 
-class TutorialItem(scrapy.Item):
+class BooksItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
