63682b1fd70b856ccdf33f1d9954b217614e0836
Awais Hassan <awaishassan43@gmail.com>
2021-11-11 17:04:52 +0500

    Crawl spider completed

--- books/books/spiders/spider.py
+++ books/books/spiders/spider.py
@@ -1,72 +1,57 @@
-# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 import scrapy
 
 
-class SpiderSpider(scrapy.Spider):
+class SpiderSpider(CrawlSpider):
     name = 'spider'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://books.toscrape.com/']
     base_url = 'http://books.toscrape.com/'
 
-    def parse(self, response):
-        all_books = response.xpath('//article[@class="product_pod"]')
+    rules = [Rule(LinkExtractor(allow='catalogue'),
+                  callback='parse_filter', follow=True)]
 
-        for book in all_books:
-            book_url = book.xpath('.//h3/a/@href').extract_first()
+    def parse_filter(self, response):
+        exists = response.xpath('//div[@id="product_gallery"]').extract_first()
+        if exists:
+            title = response.xpath('//div/h1/text()').extract_first()
 
-            if 'catalogue/' not in book_url:
-                book_url = 'catalogue/' + book_url
+            relative_image = response.xpath(
+                '//div[@class="item active"]/img/@src').extract_first().replace('../..', '')
+            final_image = self.base_url + relative_image
 
-            book_url = self.base_url + book_url
+            price = response.xpath(
+                '//div[contains(@class, "product_main")]/p[@class="price_color"]/text()').extract_first()
+            stock = response.xpath(
+                '//div[contains(@class, "product_main")]/p[contains(@class, "instock")]/text()').extract()[1].strip()
+            stars = response.xpath(
+                '//div/p[contains(@class, "star-rating")]/@class').extract_first().replace('star-rating ', '')
+            description = response.xpath(
+                '//div[@id="product_description"]/following-sibling::p/text()').extract_first()
+            upc = response.xpath(
+                '//table[@class="table table-striped"]/tr[1]/td/text()').extract_first()
+            price_excl_tax = response.xpath(
+                '//table[@class="table table-striped"]/tr[3]/td/text()').extract_first()
+            price_inc_tax = response.xpath(
+                '//table[@class="table table-striped"]/tr[4]/td/text()').extract_first()
+            tax = response.xpath(
+                '//table[@class="table table-striped"]/tr[5]/td/text()').extract_first()
+            number_of_reviews = response.xpath(
+                '//table[@class="table table-striped"]/tr[5]/td/text()').extract_first().replace('\u00a3', '')
 
-            yield scrapy.Request(book_url, callback=self.parse_book)
-
-        next_page_partial_url = response.xpath(
-            '//li[@class="next"]/a/@href').extract_first()
-
-        if next_page_partial_url:
-            if 'catalogue/' not in next_page_partial_url:
-                next_page_partial_url = "catalogue/" + next_page_partial_url
-
-            next_page_url = self.base_url + next_page_partial_url
-            yield scrapy.Request(next_page_url, callback=self.parse)
-
-    def parse_book(self, response):
-        title = response.xpath('//div/h1/text()').extract_first()
-
-        relative_image = response.xpath(
-            '//div[@class="item active"]/img/@src').extract_first().replace('../..', '')
-        final_image = self.base_url + relative_image
-
-        price = response.xpath(
-            '//div[contains(@class, "product_main")]/p[@class="price_color"]/text()').extract_first()
-        stock = response.xpath(
-            '//div[contains(@class, "product_main")]/p[contains(@class, "instock")]/text()').extract()[1].strip()
-        stars = response.xpath(
-            '//div/p[contains(@class, "star-rating")]/@class').extract_first().replace('star-rating ', '')
-        description = response.xpath(
-            '//div[@id="product_description"]/following-sibling::p/text()').extract_first()
-        upc = response.xpath(
-            '//table[@class="table table-striped"]/tr[1]/td/text()').extract_first()
-        price_excl_tax = response.xpath(
-            '//table[@class="table table-striped"]/tr[3]/td/text()').extract_first()
-        price_inc_tax = response.xpath(
-            '//table[@class="table table-striped"]/tr[4]/td/text()').extract_first()
-        tax = response.xpath(
-            '//table[@class="table table-striped"]/tr[5]/td/text()').extract_first()
-        number_of_reviews = response.xpath(
-            '//table[@class="table table-striped"]/tr[5]/td/text()').extract_first().replace('\u00a3', '')
-
-        yield {
-            'Title': title,
-            'Image': final_image,
-            'Price': price,
-            'Stock': stock,
-            'Stars': stars,
-            'Description': description,
-            'Upc': upc,
-            'Price after tax': price_excl_tax,
-            'Price incl tax': price_inc_tax,
-            'Tax': tax,
-            'Number of reviews': number_of_reviews,
-        }
+            yield {
+                'Title': title,
+                'Image': final_image,
+                'Price': price,
+                'Stock': stock,
+                'Stars': stars,
+                'Description': description,
+                'Upc': upc,
+                'Price after tax': price_excl_tax,
+                'Price incl tax': price_inc_tax,
+                'Tax': tax,
+                'Number of reviews': number_of_reviews,
+            }
+        else:
+            print(response.url)
